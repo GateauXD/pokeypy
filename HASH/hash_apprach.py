e732bc8a0213b33dcd6dace0aa95d720b3dbaeb3
GateauXD <dantran2016@gmail.com>
2020-03-25 15:01:07 -0700

    Finished overall logic testing time

--- HASH/hash_apprach.py
+++ HASH/hash_apprach.py
@@ -4,20 +4,40 @@ import requests
 
 from PIL import Image
 from io import BytesIO
+from hash_handler import HashHandler
+from flash_app import keep_alive
 
 import discord
 from discord.ext import commands
 
-class BotHandler(discord.Client):
-    async def on_ready(self):
-        print("Logged in as :" + self.user)
-    
-    async def on_message(self, message):
-        # Handles check if pokemon appers
-        if self.pokeypy.check_message(message):
-            await self.pokeypy.get_pokemon_name(message)
-            return
+with open('constants.json') as s:
+    constants = json.load(s)
+client = commands.Bot(command_prefix=constants['prefix'])
+token = constants["token"]
 
+@client.event
+async def on_ready():
+    print(f'{client.user.name} logged in!')
 
-client = BotHandler()
-client.run('token')
+@client.event
+async def on_message(message):
+    # Checking if the message is from pokebot
+    if message.author.id == 365975655608745985:
+        with open('constants.json') as s:
+            constants = json.load(s)
+        
+        # Checking if in allowed channel
+        if message.channel.id in constants["allowed_channels_ids"]:
+            # Checking if there is an image embed in the message
+            if message.embeds:
+                if 'Guess the' in message.embed[0].description:
+                    url = message.embed[0].image.url
+                    pokemon_name = HashHandler(url).start()
+
+                    await asyncio.sleep(random.randint(1, 5))
+
+                    await message.channel.send("p!catch " + name)
+        await client.process_commands(message)
+
+keep_alive()
+client.run(token,bot=False)
