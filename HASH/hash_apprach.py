fcf2b1a30d7c69d1f840a460107d00523a45e97d
GateauXD <dantran2016@gmail.com>
2020-03-25 14:12:08 -0700

    Moving message handler into the main file

--- HASH/hash_apprach.py
+++ HASH/hash_apprach.py
@@ -10,4 +10,14 @@ from io import BytesIO
 import discord
 from discord.ext import commands
 
-#TODO Add main runing scripts here
+class BotHandler(discord.Client):
+    async def on_ready(self):
+        print("Logged in as :" + self.user)
+    
+    async def on_message(self, message):
+        # Handles check if pokemon appers
+        if self.pokeypy.check_message(message):
+            await self.pokeypy.get_pokemon_name(message)
+            return
+
+        await self.client.process_commands(message)
