4a716670fc5d47deb198f169d09e51ef24fd9cd5
GateauXD <dantran2016@gmail.com>
2020-03-26 12:33:53 -0700

    Added a spam bot

--- HASH/hash_apprach.py
+++ HASH/hash_apprach.py
@@ -1,5 +1,6 @@
+import string
 import random
-import time
+import asyncio
 import requests
 import json
 
@@ -17,7 +18,16 @@ token = constants["token"]
 
 @client.event
 async def on_ready():
+    with open('constants.json') as s:
+        constants = json.load(s)
+
     print(f'{client.user.name} logged in!')
+    channel = client.get_channel(constants["allowed_channels_ids"][0])
+    while True:
+        letters = string.ascii_lowercase
+        spam_text = ''.join(random.choice(letters) for i in range(random.randint(5,20)))
+        await channel.send(spam_text)
+        await asyncio.sleep(random.uniform(0,2))
 
 @client.event
 async def on_message(message):
@@ -34,7 +44,7 @@ async def on_message(message):
                     url = message.embeds[0].image.url
                     pokemon_name = HashHandler(url).get_pokemon_name()
 
-                    await time.sleep(random.randint(1,3))
+                    await asyncio.sleep(random.randint(1,3))
 
                     await message.channel.send("p!catch " + pokemon_name)
         await client.process_commands(message)
